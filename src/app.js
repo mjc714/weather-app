5daff096c1e8355b355870dcc0527a7ad350c9ac
Matthew Chin <chin.matthew1212@gmail.com>
2018-01-15 18:59:59 -0500

    Update features.
    Add yarg module.
    Create yarg options.
    Add formatting of user input to URI address.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,29 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        address: {
+            demand: true,
+            alias: 'a',
+            describe: 'Address to get weather for.',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+// Convert user input to URI formatting.
+let encodedAddress = encodeURIComponent(argv.address);
 
 // request object
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?${encodedAddress}`,
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    // Display the address, latitude, longitude from request body object.
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
