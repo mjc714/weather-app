cdb15bceb414c13d8d2081c5219208a8e27cac56
Matthew Chin <chin.matthew1212@gmail.com>
2018-01-16 19:23:27 -0500

    Add some error handling.
    Added error handling based on response body.

--- src/app.js
+++ src/app.js
@@ -22,8 +22,14 @@ request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?${encodedAddress}`,
     json: true
 }, (error, response, body) => {
-    // Display the address, latitude, longitude from request body object.
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    if (error) {
+        console.log('Unable to connect to Google server.');
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to locate address.');
+    } else if (body.status === 'OK') {
+        // Display the address, latitude, longitude from request body object.
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    }
 });
